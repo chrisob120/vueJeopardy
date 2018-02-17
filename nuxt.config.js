5a041fe2874af065d30e582313f26c2af3862705
Chris O'Brien <chris@chrisob.com>
2018-02-17 01:27:12 -0800

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: 'Jeopardy Game',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Simple jeopardy game' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    modules: [
+        'bootstrap-vue/nuxt'
+    ],
+    css: [
+        { src: '~/assets/iconic/font/css/open-iconic-bootstrap.css' }
+    ],
+    /*
+    ** Customize the progress bar color
+    */
+    loading: { color: '#3B8070' },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** Run ESLint on save
+        */
+        extend (config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue|otf)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
+    }
+};
